70e50519f046c72ccf1f27885d73aa8dfd0e857d
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2018-06-03 18:54:33 +0300

    cosmetic changes.

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,7 +3,7 @@
 angular.module('projectsApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
-      'title': 'Home',
+      'title': 'Product List',
       'link': '/'
     }];
 
